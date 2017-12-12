0872f81bf8d30109edc1352858c8f06241a98570
Anthony <anthony_bouillon@outlook.fr>
2017-12-12 12:10:27 +0100

    Les conditions

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+$magnitude = rand(1,9);
+switch ($magnitude){
+  case 1:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Micro-séisme impossible à ressentir.';
+  break;
+
+  case 2:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
+  break;
+
+  case 3:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
+  break;
+
+  case 4:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
+  break;
+
+  case 5:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.';
+  break;
+
+  case 6:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
+  break;
+
+  case 7:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
+  break;
+
+  case 8:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
+  break;
+
+  case 9:
+    echo 'Le niveau de magnitude est de ' .$magnitude. ' : Séisme capable de tout détruire sur une très vaste zone.';
+  break;
+}
+?>
